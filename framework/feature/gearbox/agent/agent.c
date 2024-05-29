77f5e4108c695507b349ec395843ab1ef1ffc696
thu-gyt <2740923038@qq.com>
2024-05-29 19:39:47 +0800

    [Bug Fix] fix ringbuffer poll (#14)

--- framework/feature/gearbox/agent/agent.c
+++ framework/feature/gearbox/agent/agent.c
@@ -82,7 +82,8 @@ int run(ConfigData *config, struct ring_buffer **rb) {
             fprintf(stdout, "Interrupted, exiting gearbox agent\n");
             ret = 0;
             break;
-        } else {
+        }
+        if(ret < 0) {
             fprintf(stderr, "Error polling ring buffer: %s\n", strerror(errno));
             break;
         }
