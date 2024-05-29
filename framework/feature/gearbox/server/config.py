3014a55672f2ee826dc8f28a19c6e986ac1c8c97
datawine <datawine97@gmail.com>
2024-05-29 06:08:47 +0000

    [Style] Cleanup server implementations (#14)

--- framework/feature/gearbox/server/config.py
+++ framework/feature/gearbox/server/config.py
@@ -1,45 +1,22 @@
 import json
 
-class AggregatorConfig:
-    def __init__(self, ip, loader_port, manager_port, collector_port):
+class ControllerConfig:
+    def __init__(self, ip, port):
         self.ip = ip
-        self.loader_port = loader_port
-        self.manager_port = manager_port
-        self.collector_port = collector_port
-
+        self.port = port
+    
     def __str__(self):
-        return f"AggregatorConfig(ip={self.ip}, loader_port={self.loader_port}, manager_port={self.manager_port}, collector_port={self.collector_port})"
-
-class ManagerConfig:
-    def __init__(self, ip, loader_port):
-        self.ip = ip
-        self.loader_port = loader_port
-
-    def __str__(self):
-        return f"ManagerConfig(ip={self.ip}, loader_port={self.loader_port})"
-
-class CollectorConfig:
-    def __init__(self, ip, manager_port):
-        self.ip = ip
-        self.manager_port = manager_port
-
-    def __str__(self):
-        return f"CollectorConfig(ip={self.ip}, manager_port={self.manager_port})"
+        return f"ControllerConfig(ip={self.ip}, port={self.port})"
 
 class Config:
-    def __init__(self, aggregators, managers, collectors):
-        self.aggregators = [AggregatorConfig(**ag) for ag in aggregators]
-        self.managers = [ManagerConfig(**m) for m in managers]
-        self.collectors = [CollectorConfig(**c) for c in collectors]
+    def __init__(self, controller):
+        self.controller = ControllerConfig(**controller)
 
     def __str__(self):
-        ag_str = ', '.join(str(ag) for ag in self.aggregators)
-        mgr_str = ', '.join(str(mgr) for mgr in self.managers)
-        col_str = ', '.join(str(col) for col in self.collectors)
-        return f"Config(aggregators=[{ag_str}], managers=[{mgr_str}], collectors=[{col_str}])"
+        return f"Config(controller=[{str(self.controller)}])"
 
 def parse_config(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
     
-    return Config(data['aggregators'], data['managers'], data['collectors'])
+    return Config(data['controller'])
--- framework/feature/gearbox/server/controller.py
+++ framework/feature/gearbox/server/controller.py
@@ -1,26 +1,11 @@
 import socket
 import threading
-from ctypes import *
-import argparse
 
-class Edge(LittleEndianStructure):
-    _pack_ = 1
-    _fields_ = [
-        #(name, ctype)
-        ('trace_id',c_uint64),
-        ('component_id1',c_uint64),
-        ('invoke_id1',c_uint16),
-        ('component_id2',c_uint64),
-        ('invoke_id2',c_uint16),
-        ('num',c_uint16)
-    ]
+from ctypes import sizeof
 
-    def encode(self):
-        return string_at(addressof(self),sizeof(self))
-    
-    def decode(self,data):
-        memmove(addressof(self),data,sizeof(self))
-        return len(data)
+from util import parse_args
+from structs import Edge
+from config import Config
 
 def handle_client(conn, addr):
     print('Connected by', addr)
@@ -33,7 +18,8 @@ def handle_client(conn, addr):
         print(f"Received edge: {edge.trace_id} {edge.component_id1} {edge.invoke_id1} {edge.component_id2} {edge.invoke_id2} {edge.num}")
     conn.close()
 
-def start_server(host='10.0.1.132', port=18080):
+def start_server(config: Config):
+    host, port = config.controller.ip, config.controller.port    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
@@ -43,11 +29,9 @@ def start_server(host='10.0.1.132', port=18080):
             thread = threading.Thread(target=handle_client, args=(conn, addr))
             thread.start()
 
+def main():
+    gearbox_config = parse_args()
+    start_server(gearbox_config)
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process some integers.")
-    parser.add_argument("--ip", help="The IP address to connect to")
-    parser.add_argument("--port", type=int, help="The port to connect to")
-
-    args = parser.parse_args()
-
-    start_server(args.ip, args.port)
+    main()
