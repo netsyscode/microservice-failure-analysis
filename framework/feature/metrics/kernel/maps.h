039c0e339e308d055d495994a4b06a339a91fbb8
thu-gyt <2740923038@qq.com>
2024-08-02 16:45:21 +0800

    [Functionalities] use perfcpu array to store metric & tag  (#14)

--- framework/feature/metrics/kernel/maps.h
+++ framework/feature/metrics/kernel/maps.h
@@ -29,5 +29,12 @@ struct {
     __uint(max_entries, 25600 * 1024 /* 2560 KB */);
 } rb SEC(".maps");
 
+struct {
+    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+    __type(key, u32);
+    __type(value, struct point);
+    __uint(max_entries, 1);
+} percpu_data_map SEC(".maps");
+
 
 
